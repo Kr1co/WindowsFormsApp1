eed8d0b2a7b0e5a2f3964ec79c9648c95db1af14
MileninaElizaveta <159778043+MileninaElizaveta@users.noreply.github.com>
2024-09-19 14:21:59 +0300

    Добавьте файлы проекта.

---
+++ MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.UsersClasses;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            textBox1.Text = "task_code_development@list.ru";
+            textBox3.Text = "Антон Игоревич";
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) ||
+                string.IsNullOrWhiteSpace(textBox3.Text) ||
+                string.IsNullOrWhiteSpace(textBox2.Text) ||
+                string.IsNullOrWhiteSpace(textBox4.Text))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+            string smtp = "smtp.mail.ru";
+            StringPair fromInfo = new StringPair("bulanov_2004@mail.ru", "Дьячков Иван Алексеевич");
+            string password = "YrFk3NVpxbPVdnbCbYvs";
+
+            StringPair toInfo = new StringPair(textBox1.Text, textBox3.Text);
+            string subject = textBox2.Text;
+            string body = $"{DateTime.Now} \n" +
+                $"{Dns.GetHostName()} \n" +
+                $"{Dns.GetHostAddresses(Dns.GetHostName()).First()} \n" +
+                $"{textBox4.Text}";
+            InfoEmailSending info =
+                new InfoEmailSending(smtp, fromInfo, password, toInfo, subject, body);
+            SendingEmail sendingEmail = new SendingEmail(info);
+            sendingEmail.Send();
+            MessageBox.Show("Письмо отправлено!");
+            foreach (TextBox textBox in Controls.OfType<TextBox>())
+                textBox.Text = "";
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
